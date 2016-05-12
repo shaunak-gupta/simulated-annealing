3f2034c8e3105d60a6ad5cd7c7d2d8636da597f0
shaunak <shaunak.gupta@gmail.com>
2016-05-12 19:57:26 +0530

    Added python script and simulation web page

---
+++ get_distances.py
@@ -0,0 +1,13 @@
+import urllib, json, time
+url = "https://maps.googleapis.com/maps/api/distancematrix/json?"
+city_string = "Amaravati|Itanagar|Dispur|Patna|Raipur|Panaji|Gandhinagar|Chandigarh|Shimla|Srinagar|Ranchi|Bengaluru|Thiruvananthapuram|Bhopal|Mumbai|Imphal|Shillong|Aizawl|Kohima|Bhubaneswar|Chandigarh|Jaipur|Gangtok|Chennai|Hyderabad|Agartala|Lucknow|Dehradun|Kolkata|Jammu|Chandigarh|Silvassa|Daman|Delhi|Pondicherry"
+cities = city_string.split("|")
+dist_mat = [[0 for x in range(len(cities))] for y in range(len(cities))] 
+for dest, city in enumerate(cities):
+	response = urllib.urlopen(url + "origins=" + city_string + "&destinations=" + city)
+	data = json.loads(response.read())
+	for source, row in enumerate(data["rows"]):
+		dist_mat[source][dest] = (row["elements"][0])["distance"]["value"]
+		print source, ",", dest, "->", (row["elements"][0])["distance"]["text"]
+	time.sleep(11)
+print dist_mat
